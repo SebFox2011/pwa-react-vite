5cb51a2f5baea96f4cba5a91127a4dfc6aea99b9
SebFox2011 <sebastien.philippe5@gmail.com>
2023-11-15 18:39:51 +0100

    Initialisation du projet

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      manifest: {
+        display:'standalone',
+        name: "React Vite PWA",
+        short_name: "VitePwaJsx",
+        description: "My Awesome App description",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
